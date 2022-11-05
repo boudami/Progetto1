42c05e7ed0f42d3bcbe89889cb482ca24a99a4ed
Amin Boudlale <AMIN.BOUDLALE@local.ittcesena.it>
2022-11-05 09:20:03 +0100

    Versione Amin

---
+++ Progetto1/Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Progetto1
+{
+    internal class Class1
+    {    
+    }
+    public class Brano
+    {
+        string titolo, autore;
+        int durata;
+        public Brano(string titolo, string autore, int durata)
+        {
+            this.titolo = titolo;
+            this.autore = autore;
+            this.durata = durata;
+        }
+        public string getTitolo()
+        {
+            return titolo;
+        }
+        public string getAutore()
+        {
+            return autore;
+        }
+        public int getDurata()
+        {
+            return durata;
+        }
+        //public Brano(string v)
+        //{
+        //    V = v;
+        //}
+
+        //Brano b= new Brano("");
+
+        //public string V { get; }
+
+    }
+}
